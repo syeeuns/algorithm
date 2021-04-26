d0d2ccbc7dc3e0009eb6873ac44aace827bbf165
syeeuns <syeeuns@gmail.com>
2021-04-26 23:45:19 +0900

    21.04.25  day11-3

--- 백준1062_가르침.py
+++ 백준1062_가르침.py
@@ -15,7 +15,7 @@ for i in range(n):
 
 arr.sort(key=len)
 arr = list(filter(lambda x: len(x) <= k, arr))
-
+print(arr)
 if k == 0:
     cnt = 0
     for one in arr:
@@ -28,28 +28,43 @@ if k == 0:
 
 else:
     cnt = 0
-
+    idx = 0
     # 길이 0인거 먼저 셈
     for one in arr:
         if len(one) == 0:
             cnt += 1
         else:
+            idx = arr.index(one)
             break
-    while k != 0:
-        for one in arr:
-            if len(one) == 0:
-                continue
 
+    # 길이 0 아닌 애들
+    teach = set()
+    for i in range(idx, len(arr)):
+        flag = True
+        length = len(arr[i])
+        for j in range(length):
+            teach.add(arr[i][j])
+            if len(teach) > k and j != length - 1:
+                flag = False
+            # elif len(teach) == k:
+            #     break
+        if flag:
+            if len(teach) > k:
+                break
+            cnt += 1
+        else:
+            break
 
+    print(cnt)
 
-
-
-# a, n, t, i, c   최소 5개는 배워야 글 읽을 수 있음
-
-# 5개 이상인 경우
-# 주어진 문자열의 문자 종류로 list 만든다
-# 위 문자 제거하고, list 만든다
-# 남은 k 만큼, 배울수있는 단어가 최대가 되도록 함
-# 길이순 오름차순 정렬
-# 젤 작은거 선택하고 돌면서 그거 다 지움
-#
+# 반례
+# 6 6
+# antarctica
+# antahellotica
+# antazzztica
+# antazzztica
+# antazzztica
+# antazzztica
+# r, z, z, z, z
+# r 1개 인식하고 끝나버림
+# 더 많은 애를 선택하도록 알고리즘 수정필요
