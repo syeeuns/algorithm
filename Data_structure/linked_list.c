08b06bd45118f280666e525ec893377fa6afa37e
syeeuns <syeeuns@gmail.com>
2021-05-04 01:03:20 +0900

    LinkedList finished

--- Data_structure/linked_list.c
+++ Data_structure/linked_list.c
@@ -6,32 +6,93 @@ struct node {
     struct node *next;
 };
 
+void insertBeginning(struct node** head, int v){
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->value = v;
+    newNode->next = (*head);
+    (*head) = newNode;
+};
+
+void insertEnd(struct node** head, int v){
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    struct node* last = (*head);
+    newNode->value = v;
+    newNode->next = NULL;
+
+    if ((*head) == NULL){
+        (*head) = newNode;
+        return;
+    }
+
+    while (last->next != NULL){
+        last = last->next;
+    }
+
+    last->next = newNode;
+    return;
+};
+
+void insertAfter(struct node* node, int v){
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->value = v;
+
+    if (node == NULL){
+        printf("the given node NULL");
+        return;
+    }
+
+    newNode->next = node->next;
+    node->next = newNode;
+};
+
+void deleteNode(struct node** head, int v){
+    struct node* temp = *head;
+    struct node* prev;
+
+    // case 1. beginnig
+    if (temp != NULL && temp->value == v){
+        (*head) = temp->next;
+        free(temp);
+        return;
+    }
+
+    // case 2. Middle
+    while (temp != NULL && temp->value != v){
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // not found key
+    if (temp == NULL){
+        printf("not found..");
+        return;
+    }
+    // found key
+    prev->next = temp->next;
+    free(temp);
+}
+
+
 void printLinkedList(struct node *p){
     while (p != NULL){
-        printf("%d\n", p->value);
+        printf("%d ----> ", p->value);
         p = p->next;
     }
 }
 
 int main()
 {
-    struct node *head;
-    struct node *one = NULL;
-    struct node *two = NULL;
-    struct node *three = NULL;
+    struct node *head = NULL;
 
-    one = malloc(sizeof(struct node));
-    two = malloc(sizeof(struct node));
-    three = malloc(sizeof(struct node));
+    insertBeginning(&head, 1);
+    insertEnd(&head, 2);
+    insertEnd(&head, 3);
+    insertAfter(head->next, 4);
+    insertBeginning(&head, 5);
 
-    one->value = 1;
-    two->value = 2;
-    three->value = 3;
 
-    head = one;
-    one->next = two;
-    two->next = three;
-    three->next = NULL;
-
+    printLinkedList(head);
+    printf("\n start delete..\n\n");
+    deleteNode(&head, 2);
     printLinkedList(head);
 }
