574bcf0e7f410ae2d266aae5a41d053bf2c222f5
syeeuns <syeeuns@gmail.com>
2021-05-06 00:46:31 +0900

    heapSort

---
+++ Data_structure & basic algorithm/sorting algorithm/heapSort.py
@@ -0,0 +1,33 @@
+arr = [4,6,0,2,1,4,5,7,9,4,2,1,4,6,4,7,9,3,2,1,3,5,6,78,8,9,6,4,2,434,67,6,84,3,31,2,24,67,1,23,4]
+# arr = [7,2,4,1,9]
+
+
+def heapify(arr, n, i):
+    largest = i
+    left = i*2 + 1
+    right = i*2 + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+
+def heapSort(arr):
+    n = len(arr)
+    # heap으로 만들기
+    for i in range(n // 2, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+
+heapSort(arr)
+n = len(arr)
+print(arr)
