63f9ed6bfbd9171b18665adcdf10a04481f48d76
syeeuns <syeeuns@gmail.com>
2021-05-11 17:44:15 +0900

    day 19-1

--- 프로그래머스_주식가격.py
+++ 프로그래머스_주식가격.py
@@ -2,8 +2,16 @@ import sys
 read = sys.stdin.readline
 
 def solution(prices):
-    answer = [0 for i in range(len(prices))]
-
+    n = len(prices)
+    answer = [0 for i in range(n)]
+    answer[n-1] = 0
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if prices[j] >= prices[i]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
     return answer
 
 
